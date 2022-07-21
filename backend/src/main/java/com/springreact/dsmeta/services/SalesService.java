0e76ab6127dd261aedfda4a74ae31bc5c8720b7a
Rodolpho Caetano <rphfti@hotmail.com>
2022-07-21 18:57:32 -0300

    API test creating Repository,Service & Controller

---
+++ backend/src/main/java/com/springreact/dsmeta/services/SalesService.java
@@ -0,0 +1,20 @@
+package com.springreact.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springreact.dsmeta.entities.Sale;
+import com.springreact.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SalesService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales() {
+		return repository.findAll();
+	}
+}
